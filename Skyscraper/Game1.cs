5204f328fc32c3ef7684d576ae694beb18d16b73
Jacob Brosler <acingmc@gmail.com>
2023-06-25 12:30:35 -0400

    Renamed template files

--- Skyscraper/Game1.cs
+++ Skyscraper/Game1.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework.Input;
 using Devcade;
 
 // MAKE SURE YOU RENAME ALL PROJECT FILES FROM DevcadeGame TO YOUR YOUR GAME NAME
-namespace DevcadeGame
+namespace Skyscraper
 {
 	public class Game1 : Game
 	{
